63eb456140a2c23692d951473b30ae2100cdfe77
p2lifau <p2lifau@gmail.com>
2022-03-01 14:55:18 -0800

    almost done

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
+// Hamburger Menu selectors
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
+});
+
+// Select modal Button elements
+const modalBtn1 = document.querySelector('.button-1');
+const modalBtn2 = document.querySelector('.button-2');
+const modalBtn3 = document.querySelector('.button-3');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+
+// Event Listeners for modal buttons
+modalBtn1.addEventListener('click', () => {
+  modalBg.classList.add('bg-active');
 })
+modalBtn2.addEventListener('click', () => {
+  modalBg.classList.add('bg-active');
+})
+modalBtn3.addEventListener('click', () => {
+  modalBg.classList.add('bg-active');
+})
+
+modalClose.addEventListener('click', () => {
+  modalBg.classList.remove('bg-active');
+})
+

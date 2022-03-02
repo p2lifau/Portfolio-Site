75494f0861cb793e027ce008804253d6e88cbeaa
p2lifau <p2lifau@gmail.com>
2022-03-01 20:35:26 -0800

    website fully responsive

--- app.js
+++ app.js
@@ -5,25 +5,27 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
 
-// Select modal Button elements
+// Select open modal elements
 const modalBtn1 = document.querySelector('.button-1');
 const modalBtn2 = document.querySelector('.button-2');
 const modalBtn3 = document.querySelector('.button-3');
+// select modal background
 const modalBg = document.querySelector('.modal-bg');
+// select close modal button
 const modalClose = document.querySelector('.modal-close');
 
 // Event Listeners for modal buttons
 modalBtn1.addEventListener('click', () => {
-  modalBg.classList.add('bg-active');
+  modalBg.classList.add('bckg-active');
 })
 modalBtn2.addEventListener('click', () => {
-  modalBg.classList.add('bg-active');
+  modalBg.classList.add('bckg-active');
 })
 modalBtn3.addEventListener('click', () => {
-  modalBg.classList.add('bg-active');
+  modalBg.classList.add('bckg-active');
 })
 
 modalClose.addEventListener('click', () => {
-  modalBg.classList.remove('bg-active');
+  modalBg.classList.remove('bckg-active');
 })
 
